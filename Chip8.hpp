49016b90601a8029b4e33c50e65642fb194da361
Andy <itzendure@gmail.com>
2020-12-28 22:36:00 -0800

    Op codes added to header

--- Chip8.hpp
+++ Chip8.hpp
@@ -23,5 +23,42 @@ public:
 private: 
     std::default_random_engine randGen;
     std::uniform_int_distribution<uint8_t> randByte;
+
+    // CHIP-8 Instructions
+    //void OP_0NNN(); // (SYS addr) Jump to a machine code routine at nnn (Ignored on modern interps)
     void OP_00E0(); // (CLS) Clear display
+    void OP_00EE(); // (RET) Return from a subroutine
+    void OP_1NNN(); // (JP addr) Sets PC to nnn
+    void OP_2NNN(); // (CALL addr) Call subroutine at nnn
+    void OP_3XKK(); // (SE Vx, byte) Skip next instruction if Vx = kk
+    void OP_4XKK(); // (SNE Vx, byte) Skip next instruction if Vx != kk
+    void OP_5XY0(); // (SE Vx, Vy) Skip next instruction if Vx = Vy
+    void OP_6XKK(); // (LD Vx, byte) Set Vx = kk
+    void OP_7XKK(); // (ADD Vx, byte) Set Vx = Vx + kk
+    void OP_8XY0(); // (LD Vx, Vy) Set Vx = Vy
+    void OP_8XY1(); // (OR Vx, Vy) Set Vx = Vx OR Vy
+    void OP_8XY2(); // (AND Vx, Vy) Set Vx = Vx AND Vy
+    void OP_8XY3(); // (XOR Vx, Vy) Set Vx = Vx XOR Vy
+    void OP_8XY4(); // (ADD Vx, Vy) Set Vx = Vx + Vy, set VF = carry
+    void OP_8XY5(); // (SUB Vx, Vy) Set Vx = Vx - Vy, set VF = NOT borrow
+    void OP_8XY6(); // (SHR Vx, Vy) Set Vx = Vx SHR 1
+    void OP_8XY7(); // (SUBN Vx, Vy) Set Vx = Vy - Vx, set VF = NOT borrow
+    void OP_8XYE(); // (SHL Vx, Vy) Set Vx = Vx SHL 1
+    void OP_9XY0(); // (SNE Vx, Vy) Skip next instruction if Vx != Vy
+    void OP_ANNN(); // (LD I, addr) Set I = nnn
+    void OP_BNNN(); // (JP V0, addr) Jump to location nnn + V0
+    void OP_CXKK(); // (RND Vx, byte) Set Vx = random byte AND kk
+    void OP_DXYN(); // (DRW Vx, Vy, nibble) Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision
+    void OP_EX9E(); // (SKP Vx) Skip next instruction if key with value of Vx is pressed
+    void OP_EXA1(); // (SKNP Vx) Skip next instruction if key with value of Vx is not pressed
+    void OP_FX07(); // (LD Vx, DT) Set Vx = delay timer value
+    void OP_FX0A(); // (LD Vx, K) Wait for a key press, store the value of the key in Vx
+    void OP_FX15(); // (LD Dt, Vx) Set delay timer = Vx
+    void OP_FX18(); // (LD ST, Vx) Set sound timer = Vx
+    void OP_FX1E(); // (ADD I, Vx) Set I = I + Vx
+    void OP_FX29(); // (LD F, Vx) Set I = location of sprite for digit Vx
+    void OP_FX33(); // (LD B, Vx) Store BCD representation of Vx in memory locations I, I+1, and I+2
+    void OP_FX55(); // (LD [I], Vx) Store registers V0 through Vx in memory starting at location I
+    void OP_FX65(); // (LD Vx, [I]) Read registers V0 through Vx from memory starting at location I
+
 };

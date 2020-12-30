3950ebd665c1fddb00a0e55e0261b9245fa1fa63
Andy <itzendure@gmail.com>
2020-12-29 16:03:52 -0800

    Added more op-codes

--- Chip8.cpp
+++ Chip8.cpp
@@ -1,8 +1,23 @@
+/**
+ * @file Chip8.cpp
+ * @author Andrew Danyliuk
+ * @brief Implementation file for a basic Chip-8 intepreter.
+ * @version 0.1
+ * @date 2020-12-29
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
 #include "Chip8.hpp"
+
 
 const unsigned int START_ADDRESS = 0x200;
 const unsigned int FONT_ADDRESS = 0x50;
+const unsigned int FIRST_TWELVE_BITS = 0xFFFU;
 const unsigned int FONTSET_SIZE = 80;
+const unsigned int REGISTER_VF = 15;
+const unsigned int REGISTER_V0 = 0;
 
 uint8_t fontset[FONTSET_SIZE] =
 {
@@ -68,4 +83,455 @@ void Chip8::LoadROM(char const* filename)
     
 }
 
+/**
+ * @brief (CLS) Clear the display.
+ * 
+ * @effects Display by setting all entries to 0.
+ * 
+ */
+void Chip8::OP_00E0()
+{
+    memset(display, 0, sizeof(display));
+}
 
+/**
+ * @brief (RET) Return from a subroutine.
+ * The interpreter sets the program counter to the address at the top of the stack, 
+ * then subtracts 1 from the stack pointer.
+ * 
+ * @effects Sets pcRegister to top of stack
+ */
+void Chip8::OP_00EE()
+{
+    pcRegister = stack[sp--];
+}
+
+/**
+ * @brief (JP addr) Jump to location nnn.
+ * The interpreter sets the program counter to nnn.
+ * 
+ * @effects sets pcRegister to opcode & b.1111.1111.1111
+ */
+void Chip8::OP_1NNN()
+{
+    pcRegister = opcode & FIRST_TWELVE_BITS; //opcode & b.1111.1111.1111
+}
+
+/**
+ * @brief Call subroutine at nnn.
+ * The interpreter increments the stack pointer, 
+ * then puts the current PC on the top of the stack. 
+ * The PC is then set to nnn.
+ * 
+ * @effects stack by setting top of stack equal to PC
+ * @effects pcRegister by equaling opcode & b.1111.1111.1111
+ */
+void Chip8::OP_2NNN()
+{
+    stack[++sp] = pcRegister;
+    pcRegister = opcode & FIRST_TWELVE_BITS; //opcode & b.1111.1111.1111
+}
+
+/**
+ * @brief Skip next instruction if Vx = kk.
+ * The interpreter compares register Vx to kk, and if they are equal, 
+ * increments the program counter by 2.
+ * 
+ * @effects pcRegister by incrementing by 2 if registers[Vx] == kk
+ */
+void Chip8::OP_3XKK()
+{
+    uint8_t Vx = (0x0F00U & opcode) >> 8U;
+    uint8_t byte = 0x0FFU & opcode;
+    if (registers[Vx] == byte)
+    {
+        pcRegister += 2; //skip instruction
+    }
+}
+
+/**
+ * @brief Skip next instruction if Vx != kk.
+ * The interpreter compares register Vx to kk, and if they are not equal, 
+ * increments the program counter by 2.
+ * 
+ * @effects pcRegister by incrementing by 2 if registers[Vx] != kk
+ */
+void Chip8::OP_4XKK()
+{
+    uint8_t Vx = (0x0F00U & opcode) >> 8U;
+    uint8_t byte = 0x0FFU & opcode;
+    if (registers[Vx] != byte)
+    {
+        pcRegister += 2; //skip instruction
+    }
+}
+
+/**
+ * @brief Skip next instruction if Vx = Vy.
+ * The interpreter compares register Vx to register Vy, 
+ * and if they are equal, increments the program counter by 2.
+ * 
+ * @effects pcRegister by incrementing by 2 if registers[Vx] == registers[Vy]
+ */
+void Chip8::OP_5XY0()
+{
+    uint8_t Vx = (0x0F00U & opcode) >> 8U;
+    uint8_t Vy = (0x00F0U & opcode) >> 4U;
+
+    if (Vx == Vy)
+        {
+            pcRegister += 2; //skip instruction
+        }
+}
+
+/**
+ * @brief Set Vx = kk.
+ * The interpreter puts the value kk into register Vx.
+ * 
+ * @effects registers[Vx] by setting = kk
+ */
+void Chip8::OP_6XKK()
+{
+    uint8_t Vx = (0x0F00U & opcode) >> 8U;
+    uint8_t byte = opcode & 0x00FFU;
+    registers[Vx] = byte;
+}
+
+/**
+ * @brief Set Vx = Vx + kk.
+ * Adds the value kk to the value of register Vx, then stores the result in Vx.
+ * 
+ * @effects registers[Vx] by adding kk to it
+ */
+void Chip8::OP_7XKK()
+{
+    uint8_t Vx = (0x0F00U & opcode) >> 8U;
+    uint8_t byte = opcode & 0x00FFU;
+
+    registers[Vx] += byte;
+}
+
+/**
+ * @brief Set Vx = Vy.
+ * Stores the value of register Vy in register Vx.
+ * 
+ * @effects registers[Vx] by setting equal to registers[Vy]
+ */
+void Chip8::OP_8XY0()
+{
+    uint8_t Vx = (0x0F00U & opcode) >> 8U;
+    uint8_t Vy = (0x00F0U & opcode) >> 4U;
+
+    registers[Vx] = registers[Vy];
+}
+
+/**
+ * @brief Set Vx = Vx OR Vy.
+ * Performs a bitwise OR on the values of Vx and Vy, then stores the result in Vx. 
+ * A bitwise OR compares the corrseponding bits from two values, 
+ * and if either bit is 1, then the same bit in the result is also 1. 
+ * Otherwise, it is 0.
+ * 
+ * @effects registers[Vx] by ORing with registers[Vy]
+ */
+void Chip8::OP_8XY1()
+{
+    uint8_t Vx = (0x0F00U & opcode) >> 8U;
+    uint8_t Vy = (0x00F0U & opcode) >> 4U;
+    
+    registers[Vx] |= registers[Vy];
+}
+
+/**
+ * @brief Set Vx = Vx AND Vy.
+ * Performs a bitwise AND on the values of Vx and Vy, then stores the result in Vx. 
+ * A bitwise AND compares the corrseponding bits from two values, 
+ * and if both bits are 1, then the same bit in the result is also 1. 
+ * Otherwise, it is 0.
+ * 
+ * @effects registers[Vx] by ANDing with registers[Vy]
+ */
+void Chip8::OP_8XY2()
+{
+    uint8_t Vx = (0x0F00U & opcode) >> 8U;
+    uint8_t Vy = (0x00F0U & opcode) >> 4U;
+
+    registers[Vx] &= registers[Vy];
+}
+
+/**
+ * @brief Set Vx = Vx XOR Vy.
+ * Performs a bitwise exclusive OR on the values of Vx and Vy, then stores the result in Vx. 
+ * An exclusive OR compares the corrseponding bits from two values, 
+ * and if the bits are not both the same, then the corresponding bit in the result is set to 1. 
+ * Otherwise, it is 0.
+ * 
+ * @effects registers[Vx] by XORing with registers[Vy]
+ */
+void Chip8::OP_8XY3()
+{
+    uint8_t Vx = (0x0F00U & opcode) >> 8U;
+    uint8_t Vy = (0x00F0U & opcode) >> 4U;
+
+    registers[Vx] ^= registers[Vy];
+}
+
+/**
+ * @brief Set Vx = Vx + Vy, set VF = carry.
+ * The values of Vx and Vy are added together. 
+ * If the result is greater than 8 bits (i.e., > 255,) VF is set to 1, otherwise 0. 
+ * Only the lowest 8 bits of the result are kept, and stored in Vx.
+ * 
+ * @effects registers[REGISTER_VF] by setting to 1 if registers[Vx] + registers[Vy] > 255 and 0 otherwise
+ * @effects registers[Vx] by equaling sum of registers[Vx] and registers[Vy]
+ */
+void Chip8::OP_8XY4()
+{
+    uint8_t Vx = (0x0F00U & opcode) >> 8U;
+    uint8_t Vy = (0x00F0U & opcode) >> 4U;
+    
+    uint16_t sum = registers[Vx] + registers[Vy];
+    
+    if (sum > 0x00FFU)
+    {
+        registers[REGISTER_VF] = 1;
+    } else
+    {
+        registers[REGISTER_VF] = 0;
+    }
+
+    registers[Vx] = sum & 0x00FFU; 
+    
+}
+
+/**
+ * @brief Set Vx = Vx - Vy, set VF = NOT borrow.
+ * If Vx > Vy, then VF is set to 1, otherwise 0. 
+ * Then Vy is subtracted from Vx, and the results stored in Vx.
+ * 
+ * @effects registers[REGISTER_VF] by setting to 1 if registers[Vx] > registers[Vy] and 0 otherwise
+ * @effects registers[Vx] by minusing registers[Vy] from registers[Vx]
+ */
+void Chip8::OP_8XY5()
+{
+    uint8_t Vx = (0x0F00U & opcode) >> 8U;
+    uint8_t Vy = (0x00F0U & opcode) >> 4U;
+
+    if (registers[Vx] > registers[Vy])
+    {
+        registers[REGISTER_VF] = 1;
+    } else
+    {
+        registers[REGISTER_VF] = 0;
+    }
+
+    registers[Vx] -= registers[Vy];
+}
+
+/**
+ * @brief Set Vx = Vx SHR 1.
+ * If the least-significant bit of Vx is 1, then VF is set to 1, otherwise 0. Then Vx is divided by 2.
+ * 
+ * @effects registers[REGISTER_VF] by setting to the least significant bit of registers[Vx]
+ * @effects registers[Vx] by shifting right by 1 bit
+ */
+void Chip8::OP_8XY6()
+{
+    uint8_t Vx = (0x0F00U & opcode) >> 8U;
+    uint8_t LSB = registers[Vx] & 1;
+
+    registers[REGISTER_VF] = LSB;
+
+    registers[Vx] >>= 1U;
+    
+}
+
+/**
+ * @brief Set Vx = Vy - Vx, set VF = NOT borrow.
+ * If Vy > Vx, then VF is set to 1, otherwise 0. 
+ * Then Vx is subtracted from Vy, and the results stored in Vx.
+ * 
+ * @effects registers[REGISTER_VF] by setting to 1 if registers[Vy] > registers[Vx], 0 otherwise
+ * @effects registers[Vx] by setting to registers[Vy] - registers[Vx]
+ */
+void Chip8::OP_8XY7()
+{
+    uint8_t Vx = (0x0F00U & opcode) >> 8U;
+    uint8_t Vy = (0x00F0U & opcode) >> 4U;
+
+    if (registers[Vy] > registers[Vx])
+    {
+        registers[REGISTER_VF] = 1;
+    } else
+    {
+        registers[REGISTER_VF] = 0;
+    }
+
+    registers[Vx] = registers[Vy] - registers[Vx];
+}
+
+/**
+ * @brief Set Vx = Vx SHL 1.
+ * If the most-significant bit of Vx is 1, then VF is set to 1, otherwise to 0. 
+ * Then Vx is multiplied by 2.
+ * 
+ * @effects registers[REGISTER_VF] by setting to the most significant bit of registers[Vx]
+ * @effects registers[Vx] by shifting left by 1 bit
+ */
+void Chip8::OP_8XYE()
+{
+    uint8_t Vx = (0x0F00U & opcode) >> 8U;
+    uint8_t MSB = (registers[Vx] & 0x80U) >> 7U;
+
+    registers[REGISTER_VF] = MSB;
+
+    registers[Vx] <<= 1;
+    
+}
+
+/**
+ * @brief Skip next instruction if Vx != Vy.
+ * The values of Vx and Vy are compared, and if they are not equal, 
+ * the program counter is increased by 2.
+ * 
+ * @effects pcRegister by incrementing by 2 if registers[Vx] != registers[Vy]
+ */
+void Chip8::OP_9XY0()
+{
+    uint8_t Vx = (0x0F00U & opcode) >> 8U;
+    uint8_t Vy = (0x00F0U & opcode) >> 4U;
+
+    if (registers[Vx] != registers[Vy])
+    {
+        pcRegister += 2;
+    }
+}
+
+/**
+ * @brief Set I = nnn.
+ * The value of register I is set to nnn.
+ * 
+ * @effects indexRegister by setting to opcode & b.1111.1111.1111
+ */
+void Chip8::OP_ANNN()
+{
+    indexRegister = opcode & 0x0FFFU;
+}
+
+/**
+ * @brief Jump to location nnn + V0.
+ * The program counter is set to nnn plus the value of V0.
+ * 
+ * @effects pcRegister by setting to registers[REGISTER_V0] + (opcode & b.1111.1111.1111)
+ */
+void Chip8::OP_BNNN()
+{
+    pcRegister = registers[REGISTER_V0] + (opcode & 0x0FFFU);
+}
+
+/**
+ * @brief Set Vx = random byte AND kk.
+ * The interpreter generates a random number from 0 to 255, which is then ANDed with the value kk. 
+ * The results are stored in Vx. See instruction OP_8XY2 for more information on AND.
+ * 
+ * @effects registers[Vx] by setting to kk ANDed with a random number between 0 to 255
+ */
+void Chip8::OP_CXKK()
+{
+    uint8_t Vx = (0x0F00U & opcode) >> 8U;
+    uint8_t byte = opcode & 0x00FFU;
+
+    registers[Vx] = byte & randByte(randGen);
+}
+
+void Chip8::OP_DXYN()
+{
+    //TODO: add this draw instruction
+}
+
+void Chip8::OP_EX9E()
+{
+    //TODO: add this
+}
+
+void Chip8::OP_EXA1()
+{
+    //TODO: add this
+}
+
+/**
+ * @brief Set Vx = delay timer value.
+ * The value of DT is placed into Vx.
+ * 
+ * @effects registers[Vx] by setting to the value of the 60 Hz delay timer
+ */
+void Chip8::OP_FX07()
+{
+    uint8_t Vx = (0x0F00U & opcode) >> 8U;
+    
+    registers[Vx] = timer;
+}
+
+void Chip8::OP_FX0A()
+{
+    //TODO: add this
+}
+
+/**
+ * @brief Set delay timer = Vx.
+ * DT is set equal to the value of Vx.
+ * 
+ * @effects timer by setting to registers[Vx]
+ */
+void Chip8::OP_FX15()
+{
+    uint8_t Vx = (0x0F00U & opcode) >> 8U;
+    
+    timer = registers[Vx];
+}
+
+/**
+ * @brief Set sound timer = Vx.
+ * ST is set equal to the value of Vx.
+ * 
+ * @effects soundTimer by setting equal to registers[Vx]
+ */
+void Chip8::OP_FX18()
+{
+    uint8_t Vx = (0x0F00U & opcode) >> 8U;
+
+    soundTimer = registers[Vx];
+}
+
+/**
+ * @brief Set I = I + Vx.
+ * The values of I and Vx are added, and the results are stored in I.
+ * 
+ * @effects indexRegister by adding registers[Vx]
+ */
+void Chip8::OP_FX1E()
+{
+    uint8_t Vx = (0x0F00U & opcode) >> 8U;
+
+    indexRegister += registers[Vx]
+}
+
+void Chip8::OP_FX29()
+{
+    //TODO: add this
+}
+
+void Chip8::OP_FX33()
+{
+    //TODO: add this
+}
+
+void Chip8::OP_FX55()
+{
+    //TODO: add this
+}
+
+void Chip8::OP_FX65()
+{
+    //TODO: add this
+}
--- Chip8.hpp
+++ Chip8.hpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <chrono>
 #include <random>
+#include <stack>
 
 class Chip8
 {
@@ -17,7 +18,7 @@ public:
     uint8_t timer{}; //60 hz timer
     uint8_t soundTimer{}; //60 hz timer for sound output
     uint8_t keypad[16]{}; //16 keys, 0 through F
-    uint32_t video[32 * 64]{}; //32 by 64 pixel video display
+    uint32_t display[32 * 64]{}; //32 by 64 pixel video display
     uint16_t opcode{}; //stores the op code that maps to the opcode on a chip-8
 
 private: 
@@ -25,40 +26,107 @@ private:
     std::uniform_int_distribution<uint8_t> randByte;
 
     // CHIP-8 Instructions
-    //void OP_0NNN(); // (SYS addr) Jump to a machine code routine at nnn (Ignored on modern interps)
-    void OP_00E0(); // (CLS) Clear display
-    void OP_00EE(); // (RET) Return from a subroutine
-    void OP_1NNN(); // (JP addr) Sets PC to nnn
-    void OP_2NNN(); // (CALL addr) Call subroutine at nnn
-    void OP_3XKK(); // (SE Vx, byte) Skip next instruction if Vx = kk
-    void OP_4XKK(); // (SNE Vx, byte) Skip next instruction if Vx != kk
-    void OP_5XY0(); // (SE Vx, Vy) Skip next instruction if Vx = Vy
-    void OP_6XKK(); // (LD Vx, byte) Set Vx = kk
-    void OP_7XKK(); // (ADD Vx, byte) Set Vx = Vx + kk
-    void OP_8XY0(); // (LD Vx, Vy) Set Vx = Vy
-    void OP_8XY1(); // (OR Vx, Vy) Set Vx = Vx OR Vy
-    void OP_8XY2(); // (AND Vx, Vy) Set Vx = Vx AND Vy
-    void OP_8XY3(); // (XOR Vx, Vy) Set Vx = Vx XOR Vy
-    void OP_8XY4(); // (ADD Vx, Vy) Set Vx = Vx + Vy, set VF = carry
-    void OP_8XY5(); // (SUB Vx, Vy) Set Vx = Vx - Vy, set VF = NOT borrow
-    void OP_8XY6(); // (SHR Vx, Vy) Set Vx = Vx SHR 1
-    void OP_8XY7(); // (SUBN Vx, Vy) Set Vx = Vy - Vx, set VF = NOT borrow
-    void OP_8XYE(); // (SHL Vx, Vy) Set Vx = Vx SHL 1
-    void OP_9XY0(); // (SNE Vx, Vy) Skip next instruction if Vx != Vy
-    void OP_ANNN(); // (LD I, addr) Set I = nnn
-    void OP_BNNN(); // (JP V0, addr) Jump to location nnn + V0
-    void OP_CXKK(); // (RND Vx, byte) Set Vx = random byte AND kk
-    void OP_DXYN(); // (DRW Vx, Vy, nibble) Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision
-    void OP_EX9E(); // (SKP Vx) Skip next instruction if key with value of Vx is pressed
-    void OP_EXA1(); // (SKNP Vx) Skip next instruction if key with value of Vx is not pressed
-    void OP_FX07(); // (LD Vx, DT) Set Vx = delay timer value
-    void OP_FX0A(); // (LD Vx, K) Wait for a key press, store the value of the key in Vx
-    void OP_FX15(); // (LD Dt, Vx) Set delay timer = Vx
-    void OP_FX18(); // (LD ST, Vx) Set sound timer = Vx
-    void OP_FX1E(); // (ADD I, Vx) Set I = I + Vx
-    void OP_FX29(); // (LD F, Vx) Set I = location of sprite for digit Vx
-    void OP_FX33(); // (LD B, Vx) Store BCD representation of Vx in memory locations I, I+1, and I+2
-    void OP_FX55(); // (LD [I], Vx) Store registers V0 through Vx in memory starting at location I
-    void OP_FX65(); // (LD Vx, [I]) Read registers V0 through Vx from memory starting at location I
+      
+    // (CLS) Clear display
+    void OP_00E0(); 
+    
+    // (RET) Return from a subroutine
+    void OP_00EE(); 
+    
+    // (JP addr) Sets PC to nnn
+    void OP_1NNN(); 
+    
+    // (CALL addr) Call subroutine at nnn
+    void OP_2NNN(); 
+    
+    // (SE Vx, byte) Skip next instruction if Vx = kk
+    void OP_3XKK(); 
+    
+    // (SNE Vx, byte) Skip next instruction if Vx != kk
+    void OP_4XKK(); 
+    
+    // (SE Vx, Vy) Skip next instruction if Vx = Vy
+    void OP_5XY0(); 
+    
+    // (LD Vx, byte) Set Vx = kk
+    void OP_6XKK(); 
+    
+    // (ADD Vx, byte) Set Vx = Vx + kk
+    void OP_7XKK(); 
+    
+    // (LD Vx, Vy) Set Vx = Vy
+    void OP_8XY0(); 
+    
+    // (OR Vx, Vy) Set Vx = Vx OR Vy
+    void OP_8XY1(); 
+    
+    // (AND Vx, Vy) Set Vx = Vx AND Vy
+    void OP_8XY2(); 
+    
+    // (XOR Vx, Vy) Set Vx = Vx XOR Vy
+    void OP_8XY3(); 
+    
+    // (ADD Vx, Vy) Set Vx = Vx + Vy, set VF = carry
+    void OP_8XY4(); 
+    
+    // (SUB Vx, Vy) Set Vx = Vx - Vy, set VF = NOT borrow
+    void OP_8XY5(); 
+    
+    // (SHR Vx, Vy) Set Vx = Vx SHR 1
+    void OP_8XY6(); 
+    
+    // (SUBN Vx, Vy) Set Vx = Vy - Vx, set VF = NOT borrow
+    void OP_8XY7(); 
+    
+    // (SHL Vx, Vy) Set Vx = Vx SHL 1
+    void OP_8XYE(); 
+    
+    // (SNE Vx, Vy) Skip next instruction if Vx != Vy
+    void OP_9XY0(); 
+    
+    // (LD I, addr) Set I = nnn
+    void OP_ANNN(); 
+    
+    // (JP V0, addr) Jump to location nnn + V0
+    void OP_BNNN(); 
+    
+    // (RND Vx, byte) Set Vx = random byte AND kk
+    void OP_CXKK(); 
+    
+    // (DRW Vx, Vy, nibble) Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision
+    void OP_DXYN(); 
+    
+    // (SKP Vx) Skip next instruction if key with value of Vx is pressed
+    void OP_EX9E(); 
+    
+    // (SKNP Vx) Skip next instruction if key with value of Vx is not pressed
+    void OP_EXA1(); 
+    
+    // (LD Vx, DT) Set Vx = delay timer value
+    void OP_FX07(); 
+    
+    // (LD Vx, K) Wait for a key press, store the value of the key in Vx
+    void OP_FX0A(); 
+    
+    // (LD Dt, Vx) Set delay timer = Vx
+    void OP_FX15(); 
+    
+    // (LD ST, Vx) Set sound timer = Vx
+    void OP_FX18(); 
+    
+    // (ADD I, Vx) Set I = I + Vx
+    void OP_FX1E(); 
+    
+    // (LD F, Vx) Set I = location of sprite for digit Vx
+    void OP_FX29(); 
+    
+    // (LD B, Vx) Store BCD representation of Vx in memory locations I, I+1, and I+2
+    void OP_FX33(); 
+    
+    // (LD [I], Vx) Store registers V0 through Vx in memory starting at location I
+    void OP_FX55(); 
+    
+    // (LD Vx, [I]) Read registers V0 through Vx from memory starting at location I
+    void OP_FX65(); 
 
 };
